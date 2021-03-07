a1e2cfe5e77b2b8ead070c998877d7f1a28c3b18
Walker53108 <sakshamkumarprasad03@gmail.com>
2021-03-07 09:32:00 +0530

    HTML CSS

--- Library/settings.py
+++ Library/settings.py
@@ -123,6 +123,7 @@ USE_TZ = True
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/3.1/howto/static-files/
 
-STATIC_URL = '/static/'
+STATIC_URL='/templates/static/'
+STATICFILES_DIRS=['/templates/static/']
 
 LOGIN_REDIRECT_URL='home'
